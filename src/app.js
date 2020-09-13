0a38b91f5f97f6cce6c5e9d88c1b886a6c1aadf1
Muhammad Faisal Tariq <mfaisaltariq@gmail.com>
2020-09-13 23:49:39 +0500

    ES6 Explored

--- src/app.js
+++ src/app.js
@@ -4,19 +4,21 @@ console.log("app running");
 
 // JSX - JavaScript XML
 
-var app = {
+const app = {
     title: "Indecision App",
     subtitle: "Let the computer make decisions.",
-    options: ['One', 'Two']
+    options: ['One', 'Two'],
+    getListItems() {
+        return this.options.map((item) => <li>Item {item}</li>)
+    }
 }
-var template = ( 
+const template = ( 
     <div>
         <h1>{app.title}</h1>
         {app.subtitle && <p>{app.subtitle}</p>}
         <p>{app.options.length > 0 ? 'Here are your options': 'No Options'}</p>
         <ol>
-            <li>Item {app.options[0]}</li>
-            <li>Item {app.options[1]}</li>
+            {app.getListItems()}
         </ol>
     </div> 
 );
@@ -25,18 +27,18 @@ var template = (
 // var age = 25;
 // var loc = 'Islamabad'
 
-var user = {
+const user = {
     name: 'Faisal',
     age:    15,
     location: 'Islamabad'
 }
 
-function getLocation(location ) {
+const getLocation = (location ) => {
     if (location) {
         return <p>Location: {location}</p>
     }
 }
-var templateTwo = (
+const templateTwo = (
     <div>
         <h1>{user.name ? user.name : 'Anonymous'}</h1>
         {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
@@ -44,6 +46,6 @@ var templateTwo = (
     </div>
 )
 
-var element = document.getElementById("app");
+const element = document.getElementById("app");
 
 ReactDOM.render(template, element);
