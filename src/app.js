19232372ef382ecd8db438b80220b836f98a23d2
Muhammad Faisal Tariq <mfaisaltariq@gmail.com>
2020-09-13 19:36:05 +0500

    Working with variables

--- src/app.js
+++ src/app.js
@@ -3,10 +3,15 @@
 console.log("app running");
 
 // JSX - JavaScript XML
+
+var app = {
+    title: "Indecision App",
+    subtitle: "Let the computer make decisions."
+}
 var template = ( 
     <div>
-        <h1>Indecision App</h1>
-        <p>This is JSX from Babel23</p>
+        <h1>{app.title}</h1>
+        <p>{app.subtitle}</p>
         <ol>
             <li>Item One</li>
             <li>Item Two</li>
@@ -14,14 +19,23 @@ var template = (
     </div> 
 );
 
+// var username = 'Faisal';
+// var age = 25;
+// var loc = 'Islamabad'
+
+var user = {
+    name: 'Faisal',
+    age:    25,
+    location: 'Islamabad'
+}
 var templateTwo = (
     <div>
-        <h1>Faisal</h1>
-        <p>Age: 25</p>
-        <p>Location: Islamabad</p>
+        <h1>{user.name}</h1>
+        <p>Age: {user.age}</p>
+        <p>Location: {user.location}</p>
     </div>
 )
 
 var element = document.getElementById("app");
 
-ReactDOM.render(templateTwo, element);
+ReactDOM.render(template, element);
