e7e252b081de833b5099786d724a620440e1e3b8
Muhammad Faisal Tariq <mfaisaltariq@gmail.com>
2020-09-17 02:02:52 +0500

    Nesting React Components and Props explored

--- src/app.js
+++ src/app.js
@@ -1,19 +1,38 @@
+class IndecisionApp extends React.Component {
+    render() {
+        const title = 'Indecision App'
+        const subTitle = "Let the computer make decisions"
+        let options = ['Make Tea', 'Play Fifa']
+        return (
+            <div>
+                <Header title={title} subTitle={subTitle}/>
+                <Action />
+                <Options options={options}/> 
+                <AddOption />
+            </div>
+        )
+    }
+}
+
 class Header extends React.Component {
     render() {
         return (
             <div>
-                <h1>Indecision App</h1>
-                <h2>Let the computer make decisions</h2>
+                <h1>{this.props.title}</h1>
+                <h2>{this.props.subTitle}</h2>
             </div>
         )
     }
 }
 
 class Action extends React.Component {
+    pickRandom() {
+        alert("Pick some random option")
+    }
     render() {
         return (
             <div>
-            <button>What should I do?</button>
+            <button onClick={this.pickRandom}>What should I do?</button>
             </div>
         )
     }
@@ -23,21 +42,40 @@ class Options extends React.Component {
     render() {
         return (
             <div>
-                <ol>    
-                <li>Option 1</li>
-                <li>Option 2</li>
-                </ol>
+                {
+                    this.props.options.map((option) => <Option key={option} optionText={option}/>) 
+                } 
             </div>
         )
     }
 }
 
-const jsx = (
-    <div>
-        <Header />
-        <Action />
-        <Options />
-    </div>
-)
+class Option extends React.Component {
+    render() {
+        return (
+            <div>
+                {this.props.optionText}
+            </div>
+        )
+    }   
+}
 
-ReactDOM.render(jsx, document.getElementById('app'));
+class AddOption extends React.Component {
+    addOption(e) {
+        e.preventDefault();
+        const val = e.target.elements.option.value.trim();
+        if(val) {
+            alert(val);
+        }
+    }
+    render () {
+        return (
+            <form onSubmit={this.addOption}>
+                <input type='text' name='option'></input>
+                <button>Add Option</button>
+            </form>
+        )
+    }
+}
+
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
