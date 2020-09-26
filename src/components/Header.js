4ba006471ef2faeff6ca3e85f850500fb5680b63
Muhammad Faisal Tariq <mfaisaltariq@gmail.com>
2020-09-26 21:53:27 +0500

    Added header styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 const Header = (props) => (
-    <div>
-        <h1>{props.title}</h1>
-        {props.subTitle && <h2>{props.subTitle}</h2>}
+    <div className="header">
+        <h1 className="title">{props.title}</h1>
+        {props.subTitle && <h2 className="subtitle">{props.subTitle}</h2>}
     </div>
 )
 
