549d95148489b6234b728a7d4ee82f32fc5a20bd
Muhammad Faisal Tariq <mfaisaltariq@gmail.com>
2020-11-01 02:25:14 +0500

    Started work again

--- src/app.js
+++ src/app.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const Header = (props) => {
-    return (
-        <h3>Indecision App</h3>
-    )
-}
+import IndecisionApp from './components/IndecisionApp';
 
 const app = document.getElementById('app');
 
-ReactDOM.render(<Header />, app);
+ReactDOM.render(<IndecisionApp />, app);
